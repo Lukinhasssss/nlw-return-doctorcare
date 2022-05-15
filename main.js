f8e7f9a9c1d89ce21fdad5e0d8132c8371b21796
Lucas Monteiro <lukasmonteir0@hotmail.com>
2022-05-15 15:44:32 -0300

    feat: finish day 05

--- main.js
+++ main.js
@@ -5,6 +5,35 @@ onScroll()
 function onScroll() {
   showNavOnScroll()
   showBackToTopButtonOnScroll()
+
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
+}
+
+function activateMenuAtCurrentSection(section) {
+  const targetLine = scrollY + (innerHeight / 2)
+
+  const sectionTop = section.offsetTop
+  const sectionHeight = section.offsetHeight
+
+  const sectionTopReachOrPassesTargetLine = targetLine >= sectionTop
+
+  const sectionEndsAt = sectionTop + sectionHeight
+
+  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
+
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+  const sectionBoundaries =
+    sectionTopReachOrPassesTargetLine && !sectionEndPassedTargetLine
+
+  menuElement.classList.remove('active')
+
+  if (sectionBoundaries)
+    menuElement.classList.add('active')
 }
 
 function showNavOnScroll() {
